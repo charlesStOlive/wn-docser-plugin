60a78ebba2beedf4c4ce6420646f75f081031656
Charles <charles@notilac.fr>
2023-10-10 11:59:18 +0200

    MAJ
    Avec langue

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,32 +1,28 @@
 <?php
 
 return [
-    'app_usage' => 'Utilisation de l\'app',
-    'documentation' => 'Documentation',
-    'models' => [
-        'appdoc' => [
-            'content' => 'Utilise une data source',
-            'create' => 'Créer Doc',
-            'description' => 'Description',
-            'form_name' => 'Modifier doc',
-            'limit_users' => 'Limité à certains rôles',
-            'menu_appdocs' => 'Documentations',
-            'menu_appdocs_description' => 'créer et modifier des pages de documentations',
-            'name' => 'Nom de la page',
-            'permissions' => 'Permissions',
-            'preview_name' => 'Preview Doc',
-            'slug' => 'Slug page',
-            'title' => 'Gestion documentations',
-            'title_front' => 'Documentation',
-            'update' => 'MAJ Doc',
-            'label' => 'Appdoc',
-        ],
-    ],
-    'technical_doc' => 'Doc technique',
-    'text_intro_controller' => 'Cliquez sur une fiche documentaire dans le menu de gauche.',
     'controllers' => [
         'appdocs' => [
             'label' => 'Appdocs',
+            'app_usage' => 'Doc utilisation/intégration',
+            'tecnical_doc' => 'Docs techniques',  
+        ],
+        'text_intro' => 'Choisissez un élément dans le menu',  
+    ],
+    'models' => [
+        'appdoc' => [
+            'content' => 'Utilise une source de données',  
+            'description' => 'Description',
+            'limit_users' => 'Limité à certains rôles',
+            'menu_appdocs' => 'Documentations',
+            'menu_appdocs_description' => 'Créer et modifier des pages de documentation',  
+            'name' => 'Nom de la page',
+            'permissions' => 'Permissions',
+            'slug' => 'Slug de la page',  
+            'title_front' => 'Documentation',
+            'label' => 'Doc',
+            'roles' => 'Rôles',
         ],
     ],
 ];
+
